1ba2559ebc36ccf9c443c74b67de32745bd2213f
Haidar57 <114583545+Haidar57@users.noreply.github.com>
2022-09-28 18:52:57 +0700

    Add files via upload

---
+++ Haidar_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout << "Nama :haidar daffa" << endl;
+    cout << "Kelas :x rpl 1" << endl;
+    cout << "Nilai :95" << endl;
+}
